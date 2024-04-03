07aac2bd3e9f1d4f205b285a61bfd63b87ea9561
ahmad Tanto Wiraga <ahmadtantowiraga@gmail.com>
2024-04-03 12:14:59 +0700

    [IMPROVE] improve login

--- livecode-1-wmb/src/App.jsx
+++ livecode-1-wmb/src/App.jsx
@@ -1,13 +1,37 @@
 import { Component } from 'react';
 import Sidebar from './shared/Sidebar/Sidebar';
 import Login from './pages/Authentication/Login';
+import Dashboard from './pages/Dasboard/Dasboard';
 
 class App extends Component {
+  state={
+    page:<Dashboard />,
+    isAuthenticated: false,
+  }
+handleLogin=(component) => {
+    this.setState({page: component});
+  }
+
+  handleAuthentication=(status)=>{
+    this.setState({
+      isAuthenticated: status,
+    })
+  }
   render() {
     return (
-      <div>
-        < Login />
-      </div>
+      <>
+        {this.state.isAuthenticated ?
+              <div className='d-flex'>
+              <Sidebar navigateTo={this.handleLogin}/>
+              <main className='w-100 flex-grow-1'>
+              
+                {this.state.page}
+              </main>
+            </div>
+            :
+            <Login handleAuthentication={this.handleAuthentication}/>  
+      }
+      </>
     );
   }
 }
---
+++ livecode-1-wmb/src/pages/Dasboard/Dasboard.jsx
@@ -0,0 +1,13 @@
+import { Component } from 'react';
+
+class Dasboard extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Dasboard</h1>
+            </div>
+        );
+    }
+}
+
+export default Dasboard;
